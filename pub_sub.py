4651109d1967e1f4168c0f259182a8f27c09ff32
saisrinivas26 <ssomarou@asu.edu>
2021-04-16 23:50:20 -0700

    datastore

--- pub_sub.py
+++ pub_sub.py
@@ -64,29 +64,48 @@ def pub(project_id, topic_id):
 
 pub('cc-test-309723', 'sai_output_topic')
 
+def listenresults():
+    subscriber = pubsub_v1.SubscriberClient()
+    subscription_path = subscriber.subscription_path('cc-test-309723', 'test_Subscription')
 
-subscriber = pubsub_v1.SubscriberClient()
-subscription_path = subscriber.subscription_path('cc-test-309723', 'test_Subscription')
-
-def callback(message):
-    print(f"Received {message.data}.")
-    message.ack()
+    def callback(message):
+        print(f"Received {message.data}.")
+        message.ack()
 
 # Limit the subscriber to only have ten outstanding messages at a time.
-flow_control = pubsub_v1.types.FlowControl(max_messages=10)
+    flow_control = pubsub_v1.types.FlowControl(max_messages=10)
 
-streaming_pull_future = subscriber.subscribe(
-    subscription_path, callback=callback, flow_control=flow_control
-)
-print(f"Listening for messages on {subscription_path}..\n")
-timeout = 20.0
+    streaming_pull_future = subscriber.subscribe(
+        subscription_path, callback=callback, flow_control=flow_control
+    )
+    print(f"Listening for messages on {subscription_path}..\n")
+    timeout = 20.0
 # Wrap subscriber in a 'with' block to automatically call close() when done.
-with subscriber:
-    try:
+    with subscriber:
+        try:
         # When `timeout` is not set, result() will block indefinitely,
         # unless an exception is encountered first.
-        streaming_pull_future.result(timeout=timeout)
-    except TimeoutError:
-        streaming_pull_future.cancel()
+            streaming_pull_future.result(timeout=timeout)
+        except TimeoutError:
+            streaming_pull_future.cancel()
 
+from google.cloud import datastore
 
+# Instantiates a client
+datastore_client = datastore.Client()
+
+# The kind for the new entity
+kind = "Task"
+# The name/ID for the new entity
+name = "sampletask1"
+# The Cloud Datastore key for the new entity
+task_key = datastore_client.key(kind, name)
+
+# Prepares the new entity
+task = datastore.Entity(key=task_key)
+task["description"] = "Buy milk"
+
+# Saves the entity
+datastore_client.put(task)
+
+print(f"Saved {task.key.name}: {task['description']}")
